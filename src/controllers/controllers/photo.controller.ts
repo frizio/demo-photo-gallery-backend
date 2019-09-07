baaf7db51e8f4b4ffdbc00de6b75287f36b7ce04
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-07 23:43:09 +0200

    Code to upload the image

--- src/controllers/controllers/photo.controller.ts
+++ src/controllers/controllers/photo.controller.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
+import Photo from "../../models/Photo";
 
 export function home(req: Request, res: Response): Response {
     return res.send('Hello from demo-photo-gallery API');
 }
 
-export function createPhoto(req: Request, res: Response) {
+export async function createPhoto(req: Request, res: Response) {
     console.log('Saving photo');
-    console.log(req.body);
+    const {title, description} = req.body; 
+    const newPhoto = {
+        title: title,
+        description: description,
+        imagePath: req.file.path        
+    }
+    const photo = new Photo(newPhoto);
+    await photo.save();
     return res.json(
         {
             message: 'Photo successfull saved'
