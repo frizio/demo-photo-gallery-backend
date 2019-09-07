6f239431241cf8578d3141efd422717692e48292
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-07 23:32:13 +0200

    Multer configurations

---
+++ src/libs/multer.ts
@@ -0,0 +1,14 @@
+import multer = require("multer");
+import uuid = require("uuid/v4");
+import path from 'path';
+
+const storage = multer.diskStorage(
+    {
+        destination: 'uploads',
+        filename: (req, file, cb) => {
+            cb(null, uuid()+path.extname(file.originalname) )
+        }
+    }
+);
+
+export default multer({storage});
