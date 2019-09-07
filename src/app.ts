814199a866301694cba9d6e476accb0956b6cfe5
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-07 18:56:50 +0200

    Add morgan middleware (logs in dev mode)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
 // SETTINGS
 app.set('port', process.env.PORT || 4000);
+
+// MIDDLEWARES
+app.use(morgan('dev'));
 
 export default app;
