54b2c3fd38bd487cb287ada5d3fee21838021c82
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-07 20:12:02 +0200

    Create Mongodb connection with mongoose

---
+++ src/database.ts
@@ -0,0 +1,11 @@
+import { connect } from "mongoose";
+
+const DB_URL = 'mongodb://app_user:password@localhost:27017/demo-photo-gallery';
+
+ export async function startConnectionToMongoDb() {
+    await connect(
+        'mongodb://app_user:password@localhost:27017/demo',
+        { useNewUrlParser: true }
+    );
+    console.log('Successfully connection to Mongodb');    
+ }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import app from "./app";
+import { startConnectionToMongoDb } from "./database";
 
 
 async function main() {
+    startConnectionToMongoDb();
     const PORT = app.get('port');
     await app.listen(PORT);
     console.log(`Server on port ${PORT}`);
