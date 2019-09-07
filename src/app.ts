17b4bd4a2e3b355684a92570396dad1f5cfd5c80
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-07 19:03:18 +0200

    Add router middleware scaffold

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import indexRoutes from "./routes/index";
 
 const app = express();
 
@@ -8,5 +9,8 @@ app.set('port', process.env.PORT || 4000);
 
 // MIDDLEWARES
 app.use(morgan('dev'));
+
+// ROUTE
+app.use('/routes', indexRoutes);
 
 export default app;
